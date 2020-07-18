1198efc9b41c015e8ea2d802f3ef39d682a32109
cams2b <49157948+cams2b@users.noreply.github.com>
2020-07-18 17:53:18 -0400

    Update CharController.cs

--- ISO world builder/Assets/Scripts/CharController.cs
+++ ISO world builder/Assets/Scripts/CharController.cs
@@ -26,6 +26,7 @@ public class CharController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // test
         if (Input.anyKey)
         {
             Move();
