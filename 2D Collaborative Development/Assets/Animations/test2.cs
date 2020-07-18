ba71e75f474ee03683e8d7451ae18359501ce20e
Cameron Beeche <cabeeche@gmail.com>
2020-07-18 15:45:08 -0400

    Testing file upload

--- 2D Collaborative Development/Assets/Animations/test2.cs
+++ 2D Collaborative Development/Assets/Animations/test2.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class test2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        // Testing for github updates
+        
     }
 
     // Update is called once per frame
